e41f12d064026654bbadee1e5d98639a1d7d7279
xiaojian1202 <chenx2003@gmail.com>
2025-07-26 21:37:49 -0700

    Created Kafka listener for messages configured to the value in application.yml

--- src/main/java/com/jpmc/midascore/MidasCoreApplication.java
+++ src/main/java/com/jpmc/midascore/MidasCoreApplication.java
@@ -1,7 +1,12 @@
 package com.jpmc.midascore;
 
+import com.jpmc.midascore.foundation.Transaction;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class MidasCoreApplication {
@@ -10,4 +15,14 @@ public class MidasCoreApplication {
         SpringApplication.run(MidasCoreApplication.class, args);
     }
 
+    /*@Bean
+    public NewTopic midasTopic() {
+        return TopicBuilder.name("topic1")
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }*/
+
+    @KafkaListener(id="myID", topics="${general.kafka-topic}")
+    public void listen(Transaction transaction) {System.out.println(transaction);}
 }
