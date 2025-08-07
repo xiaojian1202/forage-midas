c9889fb7fe4fe7ca8b8d028144ce57277d56c02b
xiaojian1202 <chenx2003@gmail.com>
2025-08-06 17:13:48 -0700

    Removed comments and cleaned up core application file

--- src/main/java/com/jpmc/midascore/MidasCoreApplication.java
+++ src/main/java/com/jpmc/midascore/MidasCoreApplication.java
@@ -4,7 +4,9 @@ import com.jpmc.midascore.foundation.Transaction;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.TopicBuilder;
 
@@ -15,14 +17,11 @@ public class MidasCoreApplication {
         SpringApplication.run(MidasCoreApplication.class, args);
     }
 
-    /*@Bean
-    public NewTopic midasTopic() {
-        return TopicBuilder.name("topic1")
-                .partitions(10)
-                .replicas(1)
-                .build();
-    }*/
-
-    @KafkaListener(id="myID", topics="${general.kafka-topic}")
-    public void listen(Transaction transaction) {System.out.println(transaction);}
+    @KafkaListener(
+            groupId = "{spring.kafka.consumer.group-id}",
+            topics="${general.kafka-topic}"
+    )
+    public void listen(Transaction transaction) {
+        System.out.println(transaction);
+    }
 }
