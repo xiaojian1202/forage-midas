aba920004ece8e87d1a248b0caf5e5528c8d6199
xiaojian1202 <chenx2003@gmail.com>
2025-08-08 19:55:25 -0700

    User class for instantiating User objects.

---
+++ src/main/java/com/jpmc/midascore/entity/User.java
@@ -0,0 +1,37 @@
+package com.jpmc.midascore.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    private float balance;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+}
